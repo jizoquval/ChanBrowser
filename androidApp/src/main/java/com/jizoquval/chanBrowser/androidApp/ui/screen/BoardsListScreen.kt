3388d0b2c1838b4a698e39fc16c14ce5aac994e5
Val <botbot46@gmail.com>
2021-04-11 15:59:12 +0700

    [Linter]

--- androidApp/src/main/java/com/jizoquval/chanBrowser/androidApp/ui/screen/BoardsListScreen.kt
+++ androidApp/src/main/java/com/jizoquval/chanBrowser/androidApp/ui/screen/BoardsListScreen.kt
@@ -1,6 +1,5 @@
 package com.jizoquval.chanBrowser.androidApp.ui.screen
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
